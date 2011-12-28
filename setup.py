20a0ea5f8c1450cfd57f1fa9dcd0b345959c23a3
Kenneth Reitz <me@kennethreitz.com>
2011-12-28 01:27:33 -0500

    setuptools

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+required = []
+setup(
+    name='certifi',
+    version='0.0.1',
+    description='Mozilla\'s SSL Certs.',
+    long_description=open('README.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='http://python-requests.org',
+    packages=[
+        'certifi',
+    ],
+    # package_dir={'mypkg': 'src/mypkg'},
+    data_files=[('certifi', ['cacert.pem'])],
+    include_package_data=True,
+    license='ISC',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+    ),
+)

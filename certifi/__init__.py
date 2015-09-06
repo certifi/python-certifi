3f774cd7ba167e2f24e1e93d02c930aed155c0f6
Cory Benfield <lukasaoz@gmail.com>
2015-09-06 15:26:39 +1200

    Provide old cert bundle.

--- certifi/__init__.py
+++ certifi/__init__.py
@@ -1,3 +1,3 @@
-from .core import where
+from .core import where, old_where
 
 __version__ = "2015.09.06"
--- certifi/core.py
+++ certifi/core.py
@@ -7,8 +7,16 @@ certifi.py
 
 This module returns the installation location of cacert.pem.
 """
-
 import os
+import warnings
+
+
+class DeprecatedBundleWarning(DeprecationWarning):
+    """
+    The weak security bundle is being deprecated. Please bother your service
+    provider to get them to stop using cross-signed roots.
+    """
+
 
 def where():
     f = os.path.split(__file__)[0]
@@ -17,6 +25,10 @@ def where():
 
 
 def old_where():
+    warnings.warn(
+        "The weak security bundle is being deprecated.",
+        DeprecatedBundleWarning
+    )
     f = os.path.split(__file__)[0]
     return os.path.join(f, 'weak.pem')
 

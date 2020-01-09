190147e8ced4c92267a14c020bae63c1fdb7f77b
nikokio <sales@cailleaux.org>
2020-01-09 09:19:13 +0100

    Possibility to Overrides CA bundle location
    Using environment variable CERTIFI_CA_BUNDLE

--- certifi/core.py
+++ certifi/core.py
@@ -10,6 +10,11 @@ import os
 
 
 def where():
+
+    # Overrides CA bundle location with custom value
+    if os.environ.get('CERTIFI_CA_BUNDLE'):
+        return os.environ.get('CERTIFI_CA_BUNDLE')
+
     f = os.path.dirname(__file__)
 
     return os.path.join(f, 'cacert.pem')

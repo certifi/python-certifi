dce648e3ef4aa840f9585f8aa125ce3a3f8f33ab
Bowe Strickland <bowe@redhat.com>
2020-01-24 08:34:02 -0500

    allow PYTHON_CERTIFI_WHERE to override stock cert location

--- certifi/core.py
+++ certifi/core.py
@@ -10,6 +10,10 @@ import os
 
 
 def where():
+
+    if 'PYTHON_CERTIFI_WHERE' in os.environ:
+        return os.environ['PYTHON_CERTIFI_WHERE']
+
     f = os.path.dirname(__file__)
 
     return os.path.join(f, 'cacert.pem')

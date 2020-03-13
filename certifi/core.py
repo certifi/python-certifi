6da62848e5d041266f173b2feca28cfa9f0766b8
Adrien Ferrand <ferrand.ad@gmail.com>
2020-03-13 15:08:40 +0100

    Use a resources loader if possible

--- certifi/core.py
+++ certifi/core.py
@@ -6,7 +6,23 @@ certifi.py
 
 This module returns the installation location of cacert.pem or its contents.
 """
+import importlib
 import os
+
+try:
+    import importlib.resources
+    # Defeat lazy module importers.
+    importlib.resources.open_binary
+    _HAVE_RESOURCE_READER = True
+except ImportError:
+    _HAVE_RESOURCE_READER = False
+
+try:
+    import pkg_resources
+    # Defeat lazy module importers.
+    _HAVE_PKG_RESOURCES = True
+except ImportError:
+    _HAVE_PKG_RESOURCES = False
 
 try:
     from importlib.resources import read_text
@@ -19,12 +35,21 @@ except ImportError:
         with open(where(), "r", encoding=encoding) as data:
             return data.read()
 
+_PACKAGE_NAME = "certifi"
+_CACERT_NAME = "cacert.pem"
+
 
 def where():
-    f = os.path.dirname(__file__)
-
-    return os.path.join(f, "cacert.pem")
+    if _HAVE_PKG_RESOURCES:
+        return pkg_resources.resource_filename(_PACKAGE_NAME, _CACERT_NAME)
+    else:
+        mod = importlib.import_module(_PACKAGE_NAME)
+        path = os.path.dirname(mod.__file__)
+        return os.path.join(path, _CACERT_NAME)
 
 
 def contents():
-    return read_text("certifi", "cacert.pem", encoding="ascii")
+    if _HAVE_RESOURCE_READER:
+        return importlib.resources.read_text(_PACKAGE_NAME, _CACERT_NAME, encoding="ascii")
+    else:
+        return read_text(_PACKAGE_NAME, _CACERT_NAME, encoding="ascii")

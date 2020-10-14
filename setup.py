9da350977a877ac18a44fcf267fb77e937e40f5c
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-10-14 23:45:28 +0300

    Declare support for Python 3.9 (#138)

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     project_urls={
         'Documentation': 'https://certifiio.readthedocs.io/en/latest/',

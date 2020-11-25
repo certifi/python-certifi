fc74286112c1a36724ab11d6f7c87d41fb178c54
Alex Gaynor <alex.gaynor@gmail.com>
2020-11-25 08:50:17 -0500

    Various cleanups

--- certifi/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# certify.tests module
-
--- certifi/tests/test_certify.py
+++ certifi/tests/test_certify.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-unit tests to make sure everything behaves as expected
-"""
 
 import os
 import unittest
@@ -10,9 +7,7 @@ import certifi
 
 
 class TestCertifi(unittest.TestCase):
-
     def test_cabundle_exists(self):
-        """Check that the reported bundle exists"""
         assert os.path.exists(certifi.where())
 
     def test_read_contents(self):

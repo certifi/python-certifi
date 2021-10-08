5540a081f49a79786084c37d944def093cc26627
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-10-08 14:16:48 +0300

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     project_urls={
         'Documentation': 'https://certifiio.readthedocs.io/en/latest/',

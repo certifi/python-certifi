fc3b13d85c484e17aac8f9f7eecf8b57d81a0fbf
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-10-08 14:24:24 +0300

    Drop support for EOL Python 3.5

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     license='MPL-2.0',
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -56,7 +56,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',

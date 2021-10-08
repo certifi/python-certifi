a616491110654e9a8c58506449ebcffd6ceb7122
Nirjas Jakilim <nirjas01@student.sust.edu>
2021-10-09 00:13:57 +0600

    Updated

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Natural Language :: English',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',

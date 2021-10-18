859d80fdeb05d14b3a5398a31270844dc534659f
Jason R. Coombs <jaraco@jaraco.com>
2021-10-18 15:42:53 -0400

    Prefer setuptools alias to explicit interception.

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
-import os
-import sys
-
 import setuptools
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel upload')
-    sys.exit()
 
 if __name__ == '__main__':
     setuptools.setup()

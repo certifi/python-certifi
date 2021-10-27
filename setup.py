1c8485cf47c5abb45b4f32bfc9cc4320e88fafbd
Nirjas Jakilim <nirjas01@student.sust.edu>
2021-10-27 18:32:56 +0600

    Added Required Python Version (#152)
    * Added Required Python Version
    
    Added python_requires to specify the required python version for the library.
    
    * Updated required python version
    
    * Removed Classifiers for unsupported python versions
    
    * Updated
    
    Co-authored-by: Nirjas Jakilim <Nirzak@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     license='MPL-2.0',
+    python_requires=">=3.5",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -55,8 +56,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',

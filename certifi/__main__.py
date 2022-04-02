cf22931047f876f694fa436ad63df16813864569
sadra <lnxpylnxpy@gmail.com>
2022-04-02 13:39:34 +0430

    using if expression

--- certifi/__main__.py
+++ certifi/__main__.py
@@ -6,7 +6,4 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--contents", action="store_true")
 args = parser.parse_args()
 
-if args.contents:
-    print(contents())
-else:
-    print(where())
+print(contents() if args.contents else where())

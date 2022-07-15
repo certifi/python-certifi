47fb7ab715965684e035292d2ad3386aabdc4d25
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-07-15 16:32:24 +0300

    Fix deprecation warning on Python 3.11 (#199)
    * Fix deprecation warning on Python 3.11
    
    Fixes #192.
    
    * Retain support for Python 3.6
    
    Co-authored-by: Adam Turner <9087854+AA-Turner@users.noreply.github.com>
    
    * Bump actions versions
    
    * Use same 'where' for 3.6 and 3.7-3.10
    
    Co-authored-by: Adam Johnson <me@adamj.eu>
    Co-authored-by: Adam Turner <9087854+AA-Turner@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -54,11 +54,13 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     project_urls={
         'Source': 'https://github.com/certifi/python-certifi',

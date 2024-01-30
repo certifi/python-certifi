033e71d304a3715c4a6d9ac61e064e1a9305a463
luccabb <luccab@meta.com>
2024-01-30 16:06:47 +0000

    mypy ignore

--- certifi/core.py
+++ certifi/core.py
@@ -8,7 +8,7 @@ import sys
 import atexit
 
 def exit_cacert_ctx() -> None:
-    _CACERT_CTX.__exit__(None, None, None)  # type: ignore[attr-defined]
+    _CACERT_CTX.__exit__(None, None, None)  # type: ignore[union-attr]
 
 
 if sys.version_info >= (3, 11):

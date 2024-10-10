62f8144934352592141a5443a6b321d7d8706b33
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-10-09 19:52:39 -0600

    Added 3.13 classifier (#322)

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     project_urls={
         "Source": "https://github.com/certifi/python-certifi",
